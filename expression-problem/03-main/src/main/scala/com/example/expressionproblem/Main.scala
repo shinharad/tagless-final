2728ebf73f5ee6afcea73367991a1f60eee64aee
shinharad <shin.harad@gmail.com>
2020-08-24 09:04:10 +0900

    refactoring

--- expression-problem/03-main/src/main/scala/com/example/expressionproblem/Main.scala
+++ expression-problem/03-main/src/main/scala/com/example/expressionproblem/Main.scala
@@ -1,50 +1,57 @@
 package com.example
 package expressionproblem
 
+import scala.util.chaining._
+
 object Main extends App {
   println("-" * 100)
 
-  println(initial.Eval.interpret(initial.Program.exp))
-  println(initial.View.interpret(initial.Program.exp))
-  println(initial.View.prefix(initial.Program.exp))
+  initial.Eval.interpret(initial.Program.exp).tap(println)
+  initial.View.interpret(initial.Program.exp).tap(println)
+  initial.View.prefix(initial.Program.exp).tap(println)
   println("-" * 100)
 
   import `final`._
 
-  println(Program.Expression.dsl(Evaluate.Expression.dsl).run)
-  println(Program.Expression.dsl(View.Expression.dsl).run)
+  Program
+    .Expression
+    .dsl(Evaluate.Expression.dsl)
+    .run
+    .tap(println)
+
+  Program
+    .Expression
+    .dsl(View.Expression.dsl)
+    .run
+    .tap(println)
 
   println("-" * 100)
 
-  println(
-    Program
-      .Multiplication
-      .dsl(Evaluate.Expression.dsl, Evaluate.Multiplication.dsl)
-      .run
-  )
+  Program
+    .Multiplication
+    .dsl(Evaluate.Expression.dsl, Evaluate.Multiplication.dsl)
+    .run
+    .tap(println)
 
-  println(
-    Program
-      .Multiplication
-      .dsl(View.Expression.dsl, View.Multiplication.dsl)
-      .run
-  )
+  Program
+    .Multiplication
+    .dsl(View.Expression.dsl, View.Multiplication.dsl)
+    .run
+    .tap(println)
 
   println("-" * 100)
 
-  println(
-    Program
-      .MultiplicationInTheMiddle
-      .dsl(Evaluate.Expression.dsl, Evaluate.Multiplication.dsl)
-      .run
-  )
+  Program
+    .MultiplicationInTheMiddle
+    .dsl(Evaluate.Expression.dsl, Evaluate.Multiplication.dsl)
+    .run
+    .tap(println)
 
-  println(
-    Program
-      .MultiplicationInTheMiddle
-      .dsl(View.Expression.dsl, View.Multiplication.dsl)
-      .run
-  )
+  Program
+    .MultiplicationInTheMiddle
+    .dsl(View.Expression.dsl, View.Multiplication.dsl)
+    .run
+    .tap(println)
 
   println("-" * 100)
 }
