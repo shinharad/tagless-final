ab83230a9ab41fa350ec3a29145b957f4153330c
shinharad <shin.harad@gmail.com>
2020-08-25 20:51:27 +0900

    Id

--- expression-problem/03-main/src/main/scala/com/example/expressionproblem/Main.scala
+++ expression-problem/03-main/src/main/scala/com/example/expressionproblem/Main.scala
@@ -18,73 +18,87 @@ object Main extends App {
 
   Program
     .Expression
-    .dsl(Evaluate.Expression.dsl[Id])
-    // .dsl(Evaluate.Expression.dsl[Option])
-    // .dsl(Evaluate.Expression.dsl[Either[String, *]])
+    .dsl[Id, Int](Evaluate.Expression.dsl)
     .run
     .tap(println)
 
-  // Program
-  //   .Expression
-  //   .dsl(View.Expression.dsl)
-  //   .run
-  //   .tap(println)
+  Program
+    .Expression
+    .dsl[Id, String](View.Expression.dsl)
+    .run
+    .tap(println)
 
-  // println("-" * 100)
+  println("-" * 100)
 
-  // Program
-  //   .Multiplication
-  //   .dsl(Evaluate.Expression.dsl, Evaluate.Multiplication.dsl)
-  //   .run
-  //   .tap(println)
+  Program
+    .Multiplication
+    .dsl[Id, Int](Evaluate.Expression.dsl, Evaluate.Multiplication.dsl)
+    .run
+    .tap(println)
 
-  // Program
-  //   .Multiplication
-  //   .dsl(View.Expression.dsl, View.Multiplication.dsl)
-  //   .run
-  //   .tap(println)
+  Program
+    .Multiplication
+    .dsl[Id, String](View.Expression.dsl, View.Multiplication.dsl)
+    .run
+    .tap(println)
 
-  // println("-" * 100)
+  println("-" * 100)
 
-  // Program
-  //   .MultiplicationInTheMiddle
-  //   .dsl(Evaluate.Expression.dsl, Evaluate.Multiplication.dsl)
-  //   .run
-  //   .tap(println)
+  Program
+    .MultiplicationInTheMiddle
+    .dsl[Id, Int](Evaluate.Expression.dsl, Evaluate.Multiplication.dsl)
+    .run
+    .tap(println)
 
-  // Program
-  //   .MultiplicationInTheMiddle
-  //   .dsl(View.Expression.dsl, View.Multiplication.dsl)
-  //   .run
-  //   .tap(println)
+  Program
+    .MultiplicationInTheMiddle
+    .dsl[Id, String](View.Expression.dsl, View.Multiplication.dsl)
+    .run
+    .tap(println)
 
-  // println("-" * 100)
+  println("-" * 100)
 
-  // Program
-  //   .Division
-  //   .dsl(Evaluate.Expression.dsl, Evaluate.Multiplication.dsl, Evaluate.Division.dsl)
-  //   .run
-  //   .tap(println)
+  Program
+    .Division
+    .dsl[Either[String, *], Int](
+      Evaluate.Expression.dsl,
+      Evaluate.Multiplication.dsl,
+      Evaluate.Division.dsl
+    )
+    .run
+    .tap(println)
 
-  // Program
-  //   .Division
-  //   .dsl(View.Expression.dsl, View.Multiplication.dsl, View.Division.dsl)
-  //   .run
-  //   .tap(println)
+  Program
+    .Division
+    .dsl[Id, String](
+      View.Expression.dsl,
+      View.Multiplication.dsl,
+      View.Division.dsl
+    )
+    .run
+    .tap(println)
 
-  // println("-" * 100)
+  println("-" * 100)
 
-  // Program
-  //   .DivisionInTheMiddle
-  //   .dsl(Evaluate.Expression.dsl, Evaluate.Multiplication.dsl, Evaluate.Division.dsl)
-  //   .run
-  //   .tap(println)
+  Program
+    .DivisionInTheMiddle
+    .dsl[Either[String, *], Int](
+      Evaluate.Expression.dsl,
+      Evaluate.Multiplication.dsl,
+      Evaluate.Division.dsl
+    )
+    .run
+    .tap(println)
 
-  // Program
-  //   .DivisionInTheMiddle
-  //   .dsl(View.Expression.dsl, View.Multiplication.dsl, View.Division.dsl)
-  //   .run
-  //   .tap(println)
+  Program
+    .DivisionInTheMiddle
+    .dsl[Id, String](
+      View.Expression.dsl,
+      View.Multiplication.dsl,
+      View.Division.dsl
+    )
+    .run
+    .tap(println)
 
   println("-" * 100)
 }
