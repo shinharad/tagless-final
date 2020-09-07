bab65a6794e827a7aaca907004a48a5d150c14cb
shinharad <shin.harad@gmail.com>
2020-09-08 08:43:53 +0900

    top-level definitions

--- tagless-final-scala3/fplibrary/src/main/scala/fplibrary/DSLs.scala
+++ tagless-final-scala3/fplibrary/src/main/scala/fplibrary/DSLs.scala
@@ -7,3 +7,13 @@ trait Functor[F[_]] {
 trait Applicative[F[_]] extends Functor[F] {
   def pure[A](a: A): F[A]
 }
+
+final implicit class FunctorOps[F[_]: Functor, A](private val fa: F[A]) {
+  def map[B](ab: A => B): F[B] =
+    implicitly[Functor[F]].map(fa)(ab)
+}
+
+final implicit class AnyOps[A](private val a: A) extends AnyVal {
+  def pure[F[_]: Applicative]: F[A] =
+    implicitly[Applicative[F]].pure(a)
+}
