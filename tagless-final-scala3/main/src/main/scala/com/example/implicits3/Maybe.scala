fcda2c57c9c5295932e6e22cf7cabcc5dfef7961
shinharad <shin.harad@gmail.com>
2020-09-08 09:13:00 +0900

    the enum keyword

--- tagless-final-scala3/main/src/main/scala/com/example/implicits3/Maybe.scala
+++ tagless-final-scala3/main/src/main/scala/com/example/implicits3/Maybe.scala
@@ -3,21 +3,20 @@ package implicits3
 
 import fplibrary._
 
-sealed abstract class Maybe[+A] extends Product with Serializable
+enum Maybe[+A] {
+  case Just(a: A)
+  case Nothing
+}
+
 object Maybe {
-  final case class Just[+A](a: A) extends Maybe[A]
-  case object Nothing extends Maybe[scala.Nothing]
+  given Applicative[Maybe] {
+    override def pure[A](a: A): Maybe[A] =
+      Maybe.Just(a)
 
-  implicit val dsl: Applicative[Maybe] =
-    new Applicative[Maybe] {
-      override def pure[A](a: A): Maybe[A] =
-        Maybe.Just(a)
-
-      override def [A, B](fa: Maybe[A])map(ab: A => B): Maybe[B] =
-        fa match {
-          case Just(a) => pure(ab(a))
-          case Nothing => Nothing
-        }
-    }
-
+    override def [A, B](fa: Maybe[A])map(ab: A => B): Maybe[B] =
+      fa match {
+        case Just(a) => pure(ab(a))
+        case Nothing => Nothing
+      }
+  }
 }
