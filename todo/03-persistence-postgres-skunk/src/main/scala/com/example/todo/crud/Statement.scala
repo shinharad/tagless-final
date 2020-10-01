69c98b9df444207fa4faf56dcc583a1f7d3d81d9
shinharad <shin.harad@gmail.com>
2020-10-01 09:07:06 +0900

    part9 - delete

--- todo/03-persistence-postgres-skunk/src/main/scala/com/example/todo/crud/Statement.scala
+++ todo/03-persistence-postgres-skunk/src/main/scala/com/example/todo/crud/Statement.scala
@@ -2,6 +2,25 @@ package com.example
 package todo
 package crud
 
+import java.util.UUID
+
+import skunk._
+import skunk.implicits._
+
 object Statement {
-  
+  object Delete {
+    val all: Command[Void] =
+      sql"""
+            DELETE
+              FROM todo
+        """.command
+
+    def many(size: Int): Command[List[UUID]] =
+      sql"""
+            DELETE
+              FROM todo
+              WHERE id IN (${uuid.list(size)})
+        """.command
+
+  }
 }
