1adbb1186aaec142efe01834709dcb23e0604bcf
shinharad <shin.harad@gmail.com>
2020-10-01 09:28:11 +0900

    part9 - codecs

--- todo/03-persistence-postgres-skunk/src/main/scala/com/example/todo/crud/Statement.scala
+++ todo/03-persistence-postgres-skunk/src/main/scala/com/example/todo/crud/Statement.scala
@@ -5,9 +5,38 @@ package crud
 import java.util.UUID
 
 import skunk._
+import skunk.codec.all._
 import skunk.implicits._
 
 object Statement {
+  final implicit private class UUIDCodecOps(
+    private val uuid: Codec[UUID]
+  ) extends AnyVal {
+    def string: Codec[String] =
+      uuid.imap(_.toString)(UUID.fromString)
+  }
+
+  final implicit private class TodoDataCompanionOps(
+    private val data: Todo.Data.type
+  ) {
+    val codec: Codec[Todo.Data] =
+      (text ~ timestamp).gimap[Todo.Data]
+  }
+
+  final implicit private class TodoExistingCompanionOps(
+    private val existing: Todo.Existing.type
+  ) {
+    val codec: Codec[Todo.Existing] =
+      (uuid.string ~ Todo.Data.codec).gimap[Todo.Existing]
+  }
+
+  object Insert {
+    object WithUUID {
+      def many(size: Int): Command[List[Todo.Existing]] =
+        ???
+    }
+  }
+
   object Delete {
     val all: Command[Void] =
       sql"""
@@ -15,11 +44,11 @@ object Statement {
               FROM todo
         """.command
 
-    def many(size: Int): Command[List[UUID]] =
+    def many(size: Int): Command[List[String]] =
       sql"""
             DELETE
               FROM todo
-              WHERE id IN (${uuid.list(size)})
+              WHERE id IN (${uuid.string.list(size)})
         """.command
 
   }
