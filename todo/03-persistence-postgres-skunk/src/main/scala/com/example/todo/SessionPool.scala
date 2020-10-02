7ee719e5b617822f7a4c124e6727b74e0ca2ea6f
shinharad <shin.harad@gmail.com>
2020-10-02 09:46:30 +0900

    part10 - SessionPool

---
+++ todo/03-persistence-postgres-skunk/src/main/scala/com/example/todo/SessionPool.scala
@@ -0,0 +1,22 @@
+package com.example
+package todo
+
+import cats.syntax.option._
+
+import cats.effect._
+
+import skunk._
+
+object SessionPool {
+  // def dsl[F[_]]: Resource[F, Resource[F, skunk.Session[F]]] = ???
+  def dsl[F[_]: Concurrent: ContextShift: natchez.Trace]: SessionPool[F] =
+    Session.pooled(
+      host = "localhost",
+      port = 5432,
+      user = "user",
+      password = "password".some,
+      database = "tagless-final-todo",
+      max = 10,
+      debug = true
+    )
+}
