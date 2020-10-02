64fb6a4bd6db499c507da2636333f9228a067c09
shinharad <shin.harad@gmail.com>
2020-10-02 09:32:25 +0900

    part10 - writes

--- todo/03-persistence-postgres-skunk/src/main/scala/com/example/todo/crud/PostgresEntityGateway.scala
+++ todo/03-persistence-postgres-skunk/src/main/scala/com/example/todo/crud/PostgresEntityGateway.scala
@@ -3,7 +3,7 @@ package todo
 package crud
 
 import cats._
-import cats.syntax.all._
+import cats.implicits._
 
 object PostgresEntityGateway {
 
@@ -18,54 +18,103 @@ object PostgresEntityGateway {
   //     B: effect.Bracket[F, Throwable]
   //   ): EntityGateway[F] =
 
-  def dsl[F[_]: effect.Sync](
+  def dsl[F[_]: effect.Sync: UUIDGenerator](
       resource: effect.Resource[F, skunk.Session[F]]
-    ): EntityGateway[F] =
-    new EntityGateway[F] {
-      override def writeMany(todos: Vector[Todo]): F[Vector[Todo.Existing]] =
-        ???
+    ): F[EntityGateway[F]] =
+    F.delay {
+      new EntityGateway[F] {
+        override def writeMany(todos: Vector[Todo]): F[Vector[Todo.Existing]] =
+          todos.traverse {
+            case insert: Todo.Data =>
+              resource.use { session =>
+                session
+                  .prepare(Statement.Insert.one)
+                  .use { preparedQuery =>
+                    preparedQuery.unique(insert)
+                  }
+              }
 
-      override def readManyById(ids: Vector[String]): F[Vector[Todo.Existing]] =
-        resource.use { session =>
-          session
-            .prepare(Statement.Select.many(ids.size))
-            .use { preparedQuery =>
-              preparedQuery
-                .stream(ids.to(List), 1024)
-                .compile
-                .toVector
-            }
-        }
+            // F.genUUID
+            //   .map(id => Todo.Existing(id.toString, insert))
+            //   .flatMap { e =>
+            //     resource.use { session =>
+            //       session
+            //         .prepare(Statement.Insert.WithUUID.one)
+            //         .use { preparedCommand =>
+            //           preparedCommand
+            //             .execute(e)
+            //             .as(e)
+            //         }
+            //     }
+            //   }
 
-      override def readManyByPartialDescription(
-          partialDescription: String
-        ): F[Vector[Todo.Existing]] = ???
+            case update: Todo.Existing =>
+              resource.use { session =>
+                session
+                  .prepare(Statement.Update.one)
+                  .use { preparedQuery =>
+                    preparedQuery.unique(update)
+                  }
+              }
+          }
 
-      override def readAll: F[Vector[Todo.Existing]] =
-        resource.use { session =>
-          session
-            .execute(Statement.Select.all)
-            .map(_.to(Vector))
-        }
+        override def readManyById(
+            ids: Vector[String]
+          ): F[Vector[Todo.Existing]] =
+          resource.use { session =>
+            session
+              .prepare(Statement.Select.many(ids.size))
+              .use { preparedQuery =>
+                preparedQuery
+                  .stream(ids.to(List), ChunkSizeInBytes)
+                  .compile
+                  .toVector
+              }
+          }
 
-      override def deleteMany(todos: Vector[Todo.Existing]): F[Unit] =
-        resource.use { session =>
-          session
-            .prepare(Statement.Delete.many(todos.size))
-            .use { preparedCommand =>
-              preparedCommand
-                .execute(todos.to(List).map(_.id))
-                .void
-            }
-        }
+        override def readManyByPartialDescription(
+            partialDescription: String
+          ): F[Vector[Todo.Existing]] =
+          resource.use { session =>
+            session
+              .prepare(Statement.Select.byDescription)
+              .use { preparedQuery =>
+                preparedQuery
+                  .stream(partialDescription, ChunkSizeInBytes)
+                  .compile
+                  .toVector
+              }
+          }
 
-      override def deleteAll: F[Unit] =
-        resource.use { session =>
-          session
-            .execute(Statement.Delete.all)
-            .void
-        }
+        override def readAll: F[Vector[Todo.Existing]] =
+          resource.use { session =>
+            session
+              .execute(Statement.Select.all)
+              .map(_.to(Vector))
+          }
 
+        override def deleteMany(todos: Vector[Todo.Existing]): F[Unit] =
+          resource.use { session =>
+            session
+              .prepare(Statement.Delete.many(todos.size))
+              .use { preparedCommand =>
+                preparedCommand
+                  .execute(todos.to(List).map(_.id))
+                  .void
+              }
+          }
+
+        override def deleteAll: F[Unit] =
+          resource.use { session =>
+            session
+              .execute(Statement.Delete.all)
+              .void
+          }
+
+      }
     }
+
+  private val ChunkSizeInBytes: Int =
+    1024
 
 }
