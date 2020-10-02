342fe5a36882f1e4bd550e72773a07606bdbe4e8
shinharad <shin.harad@gmail.com>
2020-10-02 09:08:22 +0900

    part10 - skunk.Session/PreparedCommand/PreparedQuery

--- todo/03-persistence-postgres-skunk/src/main/scala/com/example/todo/crud/PostgresEntityGateway.scala
+++ todo/03-persistence-postgres-skunk/src/main/scala/com/example/todo/crud/PostgresEntityGateway.scala
@@ -3,6 +3,7 @@ package todo
 package crud
 
 import cats._
+import cats.syntax.all._
 
 object PostgresEntityGateway {
 
@@ -20,19 +21,44 @@ object PostgresEntityGateway {
         ???
 
       override def readManyById(ids: Vector[String]): F[Vector[Todo.Existing]] =
-        ???
+        resource.use { session =>
+          session
+            .prepare(Statement.Select.many(ids.size))
+            .use { preparedQuery =>
+              preparedQuery
+                .stream(ids.to(List), 1024)
+                .compile
+                .toVector
+            }
+        }
 
       override def readManyByPartialDescription(
           partialDescription: String
         ): F[Vector[Todo.Existing]] = ???
 
-      override def readAll: F[Vector[Todo.Existing]] = ???
+      override def readAll: F[Vector[Todo.Existing]] =
+        resource.use { session =>
+          session
+            .execute(Statement.Select.all)
+            .map(_.to(Vector))
+        }
 
-      override def deleteMany(todos: Vector[Todo.Existing]): F[Unit] = ???
+      override def deleteMany(todos: Vector[Todo.Existing]): F[Unit] =
+        resource.use { session =>
+          session
+            .prepare(Statement.Delete.many(todos.size))
+            .use { preparedCommand =>
+              preparedCommand
+                .execute(todos.to(List).map(_.id))
+                .void
+            }
+        }
 
       override def deleteAll: F[Unit] =
         resource.use { session =>
-          ???
+          session
+            .execute(Statement.Delete.all)
+            .void
         }
 
     }
