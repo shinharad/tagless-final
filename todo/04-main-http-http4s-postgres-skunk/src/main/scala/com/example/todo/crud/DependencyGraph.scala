84326ccdcaea4c81cd966b8af7608260a186c8de
shinharad <shin.harad@gmail.com>
2020-10-06 09:30:34 +0900

    part11 - DependencyGraph

---
+++ todo/04-main-http-http4s-postgres-skunk/src/main/scala/com/example/todo/crud/DependencyGraph.scala
@@ -0,0 +1,23 @@
+package com.example.todo.crud
+
+package com.example
+package todo
+package crud
+
+import java.time.format.DateTimeFormatter
+
+import cats._
+import cats.implicits._
+
+object DependencyGraph {
+  def dsl[F[_]: effect.Sync](
+      pattern: DateTimeFormatter,
+      resource: effect.Resource[F, skunk.Session[F]]
+    ): F[Controller[F]] =
+    PostgresEntityGateway.dsl(resource).map { gateway =>
+      Controller.dsl(
+        pattern = pattern,
+        boundary = Boundary.dsl[F](gateway)
+      )
+    }
+}
