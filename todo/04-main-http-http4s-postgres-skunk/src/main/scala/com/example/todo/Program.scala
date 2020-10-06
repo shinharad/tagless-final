573ad3cdbb573cd404ae14b9963e7a42ce07e87f
shinharad <shin.harad@gmail.com>
2020-10-06 09:15:11 +0900

    part11 - program

---
+++ todo/04-main-http-http4s-postgres-skunk/src/main/scala/com/example/todo/Program.scala
@@ -0,0 +1,22 @@
+package com.example
+package todo
+
+import java.time.format.DateTimeFormatter
+
+import cats._
+import cats.implicits._
+
+import cats.effect._
+
+object Program {
+  def dsl[F[_]: Concurrent: ContextShift: natchez.Trace]: F[Unit] =
+    SessionPool.dsl.use { resource =>
+      for {
+        server <- Server.dsl
+        _ <- server.serve
+      } yield ()
+    }
+
+  private val Pattern =
+    DateTimeFormatter.ofPattern("EEEE, MMMM d, yyyy HH:mm")
+}
