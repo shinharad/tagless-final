85df71f3b708e0090fc6f30114bcfdf9d7e1a612
shinharad <shin.harad@gmail.com>
2020-10-18 21:46:50 +0900

    error-handling - part1 - project setup

---
+++ error-handling/src/main/scala/com/example/errorhandling/Main.scala
@@ -0,0 +1,10 @@
+package com.example
+package errorhandling
+
+object Main extends App {
+  println("─" * 100)
+
+  println("hello world")
+
+  println("─" * 100)
+}
---
+++ error-handling/src/test/scala/com/example/errorhandling/ExampleSuite.scala
@@ -0,0 +1,8 @@
+package com.example
+package errorhandling
+
+final class ExampleSuite extends TestSuite {
+  test("hello world") {
+    1 shouldBe 1
+  }
+}
